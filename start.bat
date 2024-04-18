6899d0df710810ce063bb76a93835bc16b7c2f32
Pizza <wilderpizza99@gmail.com>
2024-04-18 16:24:18 +0200

    Added very basic game

---
+++ start.bat
@@ -0,0 +1,11 @@
+:start
+title Level Border Server
+@echo off
+color 7
+echo +----------------------------------------------------+
+echo :                                                    :
+echo :                     SKYPIXEL                       :
+echo :                                                    :
+echo +----------------------------------------------------+
+java -Dfile.encoding=UTF-8 -Xmx8G -jar spigot.jar
+goto start
